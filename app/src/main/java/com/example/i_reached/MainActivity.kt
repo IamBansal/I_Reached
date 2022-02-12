3c05de977735e7ac601d2b89f0ca7047a7fe614b
IamBansal <abansal11ae@gmail.com>
2022-02-12 16:42:57 +0530

    Basic Layout for App

---
+++ app/src/main/java/com/example/i_reached/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.i_reached
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.example.i_reached.fragment.HelpFragment
+import com.example.i_reached.fragment.MapFragment
+import com.example.i_reached.fragment.PlacesFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private var help : ImageView? = null
+    private var bottomNavigationView : BottomNavigationView? = null
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishAffinity()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomm)
+        bottomNavigationView?.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.mapItem -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, MapFragment()).commit()
+                }
+                R.id.location -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragment_container, PlacesFragment()).commit()
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, MapFragment()).commit()
+
+        help = findViewById(R.id.help)
+        help?.setOnClickListener {
+            if(help?.tag!! == "open"){
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, MapFragment()).commit()
+                help?.tag = "close"
+            } else {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HelpFragment()).commit()
+                help?.tag = "open"
+            }
+        }
+
+    }
+}
+
