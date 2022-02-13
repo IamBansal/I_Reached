2c304c7176ce7075da826057a2b79859e8ec9ec3
IamBansal <abansal11ae@gmail.com>
2022-02-13 17:10:05 +0530

    Added Map

--- app/src/main/java/com/example/i_reached/MainActivity.kt
+++ app/src/main/java/com/example/i_reached/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.i_reached
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
--- app/src/main/java/com/example/i_reached/fragment/MapFragment.kt
+++ app/src/main/java/com/example/i_reached/fragment/MapFragment.kt
@@ -1,12 +1,20 @@
 package com.example.i_reached.fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.i_reached.R
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.lang.Exception
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,10 +26,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MapFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
+    GoogleMap.OnMyLocationClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +39,52 @@ class MapFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+    }
+
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        Toast.makeText(context, "Grant permission to access location.", Toast.LENGTH_SHORT).show()
+//    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        googleMap = p0
+//
+//        if (ActivityCompat.checkSelfPermission(
+//                requireContext(),
+//                Manifest.permission.ACCESS_FINE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+//                requireContext(),
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//
+//            return
+//        }
+//
+//        googleMap.isMyLocationEnabled = true
+//        googleMap.setOnMyLocationButtonClickListener(this)
+//        googleMap.setOnMyLocationClickListener(this)
+
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        val myLocation = LatLng(28.852529, 77.081527)
+        googleMap.addMarker(MarkerOptions().position(myLocation).title("Marker in Delhi"))
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 15f))
+
     }
 
     override fun onCreateView(
@@ -37,6 +93,17 @@ class MapFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_map, container, false)
+
+        val mapView = view.findViewById<MapView>(R.id.map)
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        try {
+            MapsInitializer.initialize(requireContext())
+        } catch (e: Exception) {
+            e.stackTrace
+        }
+        mapView.getMapAsync(this)
+
         return view
     }
 
@@ -59,4 +126,14 @@ class MapFragment : Fragment() {
                 }
             }
     }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(context, "MyLocation button clicked", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(context, "Current location:\n", Toast.LENGTH_LONG).show()
+    }
+
 }
